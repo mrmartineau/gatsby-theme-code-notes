d543ff3121a20805a86301674ebb887f38adee7a
Zander Martineau <hi@zander.wtf>
2020-02-19 22:57:52 +0000

    feat: improve the taglist

--- gatsby-theme-code-notes/src/components/TagList.jsx
+++ gatsby-theme-code-notes/src/components/TagList.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { Box, Badge } from 'theme-ui'
 
 export const TagList = ({ tags }) => (
-  <ul>
+  <Box
+    as="ul"
+    sx={{
+      listStyleType: 'none',
+      display: 'flex',
+      p: 0,
+    }}
+  >
     {tags.map((item, index) => (
-      <li key={index}>
-        <Link to={`/tag/${encodeURI(item.toLowerCase())}`}>{item}</Link>
-      </li>
+      <Box as="li" key={index} sx={{ mr: 1 }}>
+        <Link to={`/tag/${encodeURI(item.toLowerCase())}`}>
+          <Badge>{item}</Badge>
+        </Link>
+      </Box>
     ))}
-  </ul>
+  </Box>
 )
