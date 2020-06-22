96119a9f80c5c61e03de435d2a208f7883b2afe9
Zander Martineau <hi@zander.wtf>
2020-06-22 15:49:01 +0100

    feat: add keyboard shortcut to focus search input

--- gatsby-theme-code-notes/src/components/Search/SearchInput.tsx
+++ gatsby-theme-code-notes/src/components/Search/SearchInput.tsx
@@ -1,11 +1,27 @@
 /** @jsx jsx */
-import { useContext } from 'react'
+import { useContext, useEffect, useRef } from 'react'
 import { jsx, Input, Box } from 'theme-ui'
 import { SearchContext } from './SearchProvider'
 import { GoSearch } from 'react-icons/go'
 
 export const SearchInput = () => {
   const { query, setQuery } = useContext(SearchContext)
+  const inputEl = useRef<HTMLInputElement | null>(null)
+
+  useEffect(() => {
+    window.addEventListener('keydown', (event) => {
+      if (event.isComposing || event.keyCode === 191) {
+        if (inputEl && inputEl.current) {
+          event.preventDefault()
+          inputEl.current.focus()
+        }
+      }
+    })
+
+    return () => {
+      window.removeEventListener('keydown', () => {})
+    }
+  }, [])
 
   return (
     <Box
@@ -31,7 +47,31 @@ export const SearchInput = () => {
         }}
         aria-label="Search"
         placeholder="Search notes"
+        ref={inputEl}
       />
+      <Box
+        sx={{
+          border: '1px solid',
+          color: 'input',
+          borderRadius: 'default',
+          position: 'absolute',
+          top: '50%',
+          right: 2,
+          transform: 'translateY(-50%)',
+          px: 1,
+          lineHeight: '1.1rem',
+          width: '1.2rem',
+          height: '1.2rem',
+          textAlign: 'center',
+          fontFamily: 'mono',
+          fontSize: 0,
+          userSelect: 'none',
+          pointerEvents: 'none',
+          opacity: 0.7,
+        }}
+      >
+        /
+      </Box>
     </Box>
   )
 }
--- gatsby-theme-code-notes/src/gatsby-plugin-theme-ui/forms.js
+++ gatsby-theme-code-notes/src/gatsby-plugin-theme-ui/forms.js
@@ -6,6 +6,7 @@ export const forms = {
     borderRadius: 0,
     fontFamily: 'sans',
     transition: 'all 200ms ease-in-out ',
+    lineHeight: '1.4rem',
     '&:focus': {
       boxShadow: (theme) => `0 2px 0 ${theme.colors.primary}`,
       outline: 'none',
