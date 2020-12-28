4ef4f4e875ae6383796f16bb5f60bfff08e45aa1
Zander Martineau <hi@zander.wtf>
2020-12-28 15:55:51 +0000

    feat: improve notelist item styling across all viewports

--- gatsby-theme-code-notes/src/components/NoteListItem/NoteListItem.tsx
+++ gatsby-theme-code-notes/src/components/NoteListItem/NoteListItem.tsx
@@ -28,29 +28,58 @@ export const NoteListItem: FunctionComponent<NoteListItemProps> = ({
   const noteEmoji = emoji ?? '🗒'
   return (
     <Box as="article">
-      <Link as={GatsbyLink} to={slug} variant="noteListItem" onClick={onClick}>
+      <Link
+        as={GatsbyLink}
+        to={slug}
+        variant="noteListItem"
+        onClick={onClick}
+        sx={{
+          display: [null, 'flex'],
+          justifyContent: 'space-between',
+          overflow: 'hidden',
+        }}
+      >
+        <Heading
+          as="h3"
+          variant="noteListItem"
+          sx={{ mr: '3', py: 1, minWidth: 260 }}
+        >
+          <Box as="span" sx={{ position: 'absolute', left: 1 }}>
+            {noteEmoji}
+          </Box>
+          {title}
+        </Heading>
         <Flex
           sx={{
-            justifyContent: 'space-between',
-            overflow: 'hidden',
+            alignItems: 'center',
           }}
         >
-          <Heading as="h3" variant="noteListItem" sx={{ mr: '3' }}>
-            <Box as="span" sx={{ position: 'absolute', left: 1 }}>
-              {noteEmoji}
-            </Box>
-            {title}
-          </Heading>
-          <Flex sx={{ alignItems: 'center' }}>
-            {tags && <TagList tags={tags} asLinks={false} />}
-            {showDate && dateModified && modifiedTimestamp && (
-              <time dateTime={modifiedTimestamp}>
-                <Text variant="dateModified" sx={{ ml: 2 }}>
-                  {dateModified}
-                </Text>
-              </time>
-            )}
-          </Flex>
+          {tags && (
+            <TagList
+              tags={tags}
+              asLinks={false}
+              sx={{
+                whiteSpace: 'nowrap',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+              }}
+            />
+          )}
+          {showDate && dateModified && modifiedTimestamp && (
+            <Text
+              as="time"
+              variant="dateModified"
+              dateTime={modifiedTimestamp}
+              sx={{
+                ml: 2,
+                whiteSpace: 'nowrap',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+              }}
+            >
+              {dateModified}
+            </Text>
+          )}
         </Flex>
       </Link>
     </Box>
